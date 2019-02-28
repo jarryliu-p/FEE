9c14eeb15c417f1e4a3dbca14286e0afd6a5700f
Jarry Liu <onnfzftt@163.com>
2019-02-28 20:56:59 +0800

    new 的 Promise, executor 直接执行

--- exam2.js
+++ exam2.js
@@ -1,5 +1,15 @@
+//ex1
 for (var i = 0; i < 5; i++) {
     setTimeout(function () {
         console.log(i)
     }, i * 1000)
 }
+
+//ex2
+new Promise(function(resolve, reject){
+    console.log(1);
+    resolve()
+}).then(function(){
+    console.log(2);
+})
+console.log(3)
