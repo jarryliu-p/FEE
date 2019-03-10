52e517f4f1df5b4705fd1625bfc771009504edff
liujra <onnfzftt@163.com>
2019-03-10 23:07:54 +0800

    base data type

--- exam3.js
+++ exam3.js
@@ -9,6 +9,13 @@ function test() {
         document.body.appendChild(img);
     }
 }
-test();
+//test();
 
 // 3 3
+
+// ex2
+console.log(`
+JavaScript 数据类型
+值类型(基本类型)：字符串（String）、数字(Number)、布尔(Boolean)、对空（Null）、未定义（Undefined）、Symbol。
+引用数据类型：对象(Object)、数组(Array)、函数(Function)。
+`);
